36c7526fb3c210b97295b99b1c646afa9bcae4f3
Cristina De Llano <ccfdez3@gmail.com>
2019-02-19 11:33:03 -0600

    file added

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion'
+import QuizEnd from './QuizEnd.js'
 
 let quizData = require('./quiz_data.json')
 class Quiz extends Component {
     constructor(props) {
         super(props)
-        this.state = {quiz_position: 1}
+        this.state = { quiz_position: 1 }
     }
     render() {
-        return(
-            <QuizQuestion className="QuizQuestion" quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+        const isQuizEnd = ((this.state.quiz_position - 1)=== quizData.quiz_questions.length)
+        return (
+            <div>
+                {isQuizEnd ? <QuizEnd /> :
+                <QuizQuestion className="QuizQuestion" quiz_question={quizData.quiz_questions
+                [this.state.quiz_position - 1]} />}
+            </div>
         )
     }
 }
