e3a7d620f4705be71550c6522bdc3696088a8bba
Cristina De Llano <ccfdez3@gmail.com>
2019-02-19 12:14:12 -0600

    quick-fix

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -17,7 +17,7 @@ class QuizQuestion extends Component {
                     <ul>
                         {this.props.quiz_question.answer_options.map((answer_option, index) => {
                             return <QuizQuestionButton key={index} button_text={answer_option}
-                                handleClick={this.handleClick.bind(this)} />
+                                clickHandler={this.handleClick.bind(this)} />
                         })}
                     </ul>
                 </section>
